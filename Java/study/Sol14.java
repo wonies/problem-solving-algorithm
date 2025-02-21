dc7bcd32003577523e4a452be3a2c18b725b2208
wonie <wonies@github.com>
2025-02-21 21:49:41 +0900

    programmers study-sol13, 14

---
+++ Java/study/Sol14.java
@@ -0,0 +1,35 @@
+package study;
+
+
+import java.io.*;
+import java.util.*;
+public class Sol14 {
+		private static int gcd(int a, int b)
+		{
+			int temp;
+			while (b != 0)
+			{
+				temp = b;
+				b = a % b;
+				a = temp;
+			}
+			return a;
+		}
+
+		private static int lcm(int a, int b){
+			return (a * b) / gcd(a, b);
+		}
+
+		public static int solution(int[] arr) {
+			int size = arr.length;
+			if (size == 1) return 1;
+
+			int ans = arr[0];
+			for (int i=1; i<arr.length; i++)
+			{
+				ans = lcm(ans, arr[i]);
+			}
+
+			return ans;
+		}
+}
