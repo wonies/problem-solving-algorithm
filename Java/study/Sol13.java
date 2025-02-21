6e43d872ed13011ee7b5a42c9ce5fd036c048acb
wonie <wonies@github.com>
2025-02-21 22:17:31 +0900

    programmers study-sol13, 14

---
+++ Java/study/Sol13.java
@@ -0,0 +1,21 @@
+package study;
+//프로그래머스: 멀리뛰기_edit
+
+class Sol13 {
+	public long solution(int n) {
+		long answer = 0;
+		int [] dp = new int [n+2];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+		if (n==1)
+			return 1;
+		if (n==2)
+			return 2;
+		for (int i=3; i<=n; i++)
+		{
+			dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+		}
+		return dp[n];
+	}
+}
---
+++ Java/study/Sol14.java
@@ -0,0 +1,26 @@
+package study;
+//프로그래머스 : N개의 최소공배수_edit
+
+public class Sol14 {
+	private static int gcd(int a, int b)
+	{
+		return b == 0 ? a : gcd(b, a % b);
+	}
+
+	private static int lcm(int a, int b){
+		return (a * b) / gcd(a, b);
+	}
+
+	public static int solution(int[] arr) {
+		int size = arr.length;
+		if (size == 1) return 1;
+
+		int ans = arr[0];
+		for (int i=1; i<arr.length; i++)
+		{
+			ans = lcm(ans, arr[i]);
+		}
+
+		return ans;
+	}
+}
