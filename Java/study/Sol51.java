3b0a368fb8f2065fbec814d168f8f97265bc68cb
wonie <wonies@github.com>
2025-03-14 13:25:08 +0900

    programmers-51

---
+++ Java/study/Sol51.java
@@ -0,0 +1,19 @@
+package study;
+// 2xn 타일
+import java.io.*;
+import java.util.*;
+
+class Solution {
+	public int solution(int n) {
+		int answer = 0;
+		int[] dp = new int[n+1];
+		dp[1] = 1;
+		dp[2] = 2;
+
+		for (int i = 3; i <= n; i++)
+		{
+			dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+		}
+		return dp[n];
+	}
+}
