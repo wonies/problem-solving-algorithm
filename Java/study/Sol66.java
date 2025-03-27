a928ed7730cc19191f435fc20a6bf2a6c47c4a7b
wonie <wonies@github.com>
2025-03-27 15:59:13 +0900

    Programmers<66,서버증설횟수>

---
+++ Java/study/Sol66.java
@@ -0,0 +1,34 @@
+package study;
+// 서버증설횟수
+import java.io.*;
+import java.util.*;
+
+
+class Sol66 {
+	public int solution(int[] players, int m, int k) {
+		int cnt = 0;
+		int len = players.length;
+		int[] server = new int[len + k];
+
+		for (int i=0; i<len; i++)
+		{
+			int need = players[i] / m;
+			if ( server[i] >= need )
+			{
+				continue ;
+			}
+			else
+			{
+				int realNeed = need - server[i];
+				for (int j=i; j<i+k; j++)
+				{
+					server[j] += realNeed;
+				}
+				cnt += realNeed;
+			}
+		}
+
+
+		return cnt;
+	}
+}
