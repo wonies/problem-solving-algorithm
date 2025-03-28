22466c383e3637d0381ab995e55629a163106289
wonie <wonies@github.com>
2025-03-28 12:39:54 +0900

    programmers-<sol68.서버증설횟수>

---
+++ algo-challenge/programmers-problem-solving/서버증설횟수.java
@@ -0,0 +1,31 @@
+//서버증설횟수. sol68
+
+import java.io.*;
+import java.util.*;
+
+class Solution {
+	public int solution(int[] players, int m, int k) {
+		int cnt = 0;
+		int len = players.length;
+		int[] server = new int[len + k];
+
+		for (int i=0; i<len; i++)
+		{
+			int need = players[i] / m;
+			if ( server[i] >= need )
+			{
+				continue ;
+			}
+			else
+			{
+				int realNeed = need - server[i];
+				for (int j=i; j<i+k; j++)
+				{
+					server[j] += realNeed;
+				}
+				cnt += realNeed;
+			}
+		}
+		return cnt;
+	}
+}
