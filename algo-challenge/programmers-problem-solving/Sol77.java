5ed7b2c1e4bd21a14d6319e4b3c3e42498c75f30
wonie <wonies@github.com>
2025-04-09 00:26:31 +0900

    programmers-<sol77.행렬테두리회전하기>

--- algo-challenge/programmers-problem-solving/Sol77.java
+++ algo-challenge/programmers-problem-solving/Sol77.java
@@ -4,6 +4,7 @@ class Sol77 {
 		int[][] matrix = new int[rows][columns];
 
 		int num = 0;
+		int q = 0;
 		for (int i=0; i<rows; i++)
 		{
 			for (int j=0; j<columns; j++)
@@ -11,6 +12,16 @@ class Sol77 {
 				matrix[i][j] = ++num;
 			}
 		}
+
+		//          for (int i=0; i<rows; i++)
+		//         {
+		//             for (int j=0; j<columns; j++)
+		//             {
+		//                 System.out.print(matrix[i][j] + " ");
+		//             }
+		//              System.out.println("");
+		//         }
+
 
 		for (int[] query : queries)
 		{
@@ -21,8 +32,32 @@ class Sol77 {
 			int prev = matrix[startX][startY];
 			int min = prev;
 
+			for (int i=startY + 1; i <= endY; i++)
+			{
+				int cur = matrix[startX][i];
+				matrix[startX][i] = prev;
+				prev = cur;
+				min = Math.min(min, prev);
+			}
 
-			for (int i=startX + 1; i < endX; i++)
+			for (int i=startX+1; i<=endX; i++)
+			{
+				int cur = matrix[i][endY];
+				// System.out.println("cur " + cur);
+				matrix[i][endY] = prev;
+				// System.out.print("mat : " + matrix[i][endY]);
+				prev = cur;
+				min = Math.min(min, prev);
+			}
+
+			for (int i=endY-1; i>=startY; i--)
+			{
+				int cur = matrix[endX][i];
+				matrix[endX][i] = prev;
+				prev = cur;
+				min = Math.min(min, prev);
+			}
+			for (int i=endX-1; i>=startX; i--)
 			{
 				int cur = matrix[i][startY];
 				matrix[i][startY] = prev;
@@ -30,12 +65,19 @@ class Sol77 {
 				min = Math.min(min, prev);
 			}
 
-
-
-
+			answer[q++] = min;
 
 		}
 
+
+		//          for (int i=0; i<rows; i++)
+		//         {
+		//             for (int j=0; j<columns; j++)
+		//             {
+		//                 System.out.print(matrix[i][j] + " ");
+		//             }
+		//              System.out.println("");
+		//         }
 
 
 		return answer;
